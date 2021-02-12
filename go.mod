217aac8663c0ff605922a3b61cd9c78e001502ff
NinjaAung <ninja.aung02@gmail.com>
2021-02-11 20:20:59 -0800

    v1.0

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module makesite
 
 go 1.13
+
+require (
+	github.com/mdempsky/gocode v0.0.0-20200405233807-4acdcbdea79d // indirect
+	github.com/motemen/gore v0.5.2 // indirect
+)
--- makesite.go
+++ makesite.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"os"
+	"html/template"
+)
+
+
+type post struct {
+	content string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	f, _ := os.Create("new.html")
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+	firstPost, err := ioutil.ReadFile("first-post.txt")
+	
+	err = t.Execute(f, string(firstPost))
+	if err != nil {
+		panic(err)
+	}
+	
 }
