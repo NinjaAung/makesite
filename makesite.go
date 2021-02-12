89f8fd5dad32ac69a80d36212842e1cda4438808
NinjaAung <ninja.aung02@gmail.com>
2021-02-11 20:46:52 -0800

    Added Flag: file

--- makesite.go
+++ makesite.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"flag"
 	"io/ioutil"
 	"os"
 	"html/template"
+	"strings"
 )
 
 
@@ -12,10 +15,15 @@ type post struct {
 }
 
 func main() {
-	f, _ := os.Create("new.html")
+	fmt.Println("Starting")
+	filePath := flag.String("file","post.txt","Path To html file")
+
+	flag.Parse()
+
+	filePathName := strings.Split(*filePath,".")[0]
+	f, _ := os.Create(filePathName+".html")
 	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
-	firstPost, err := ioutil.ReadFile("first-post.txt")
-	
+	firstPost, err := ioutil.ReadFile(string(*filePath))
 	err = t.Execute(f, string(firstPost))
 	if err != nil {
 		panic(err)
